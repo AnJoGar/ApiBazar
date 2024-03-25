2f427442390155e90dcf1dca3450440104994d99
AndersonJordan07 <andjor2020@gmail.com>
2024-03-25 12:41:37 -0500

    jnkn

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const itemsRouter = require('./Routes/itemsRoutes');
+const itemsRouter = require('./src/Routes/itemsRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
