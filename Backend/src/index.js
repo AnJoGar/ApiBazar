574b667a565cea6ed1a202cfa5a83a2d59c927f8
AndersonJordan07 <andjor2020@gmail.com>
2024-03-27 14:11:33 -0500

    Hjv

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,15 +1,11 @@
-//index.js
 const path = require('path');
-
-// Obtener la ruta completa del archivo products.json
-const productsFilePath = path.resolve(__dirname, 'products.json');
-
-
-console.log('Ruta del archivo products.json:', productsFilePath);
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const itemsRouter = require('./itemsRoutes');
+
+const productsFilePath = path.resolve(__dirname, 'products.json');
+console.log('Ruta del archivo products.json:', productsFilePath);
+
 const products = require('./products.json');
 
 const app = express();
@@ -18,14 +14,25 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-// Asigna la lista de productos al objeto req
-app.use((req, res, next) => {
-  req.products = products.products;
-  next();
+// Controlador para buscar productos
+app.get('/api/items', (req, res) => {
+  const query = req.query.q;
+  const filteredProducts = products.products.filter(product =>
+    product.title.toLowerCase().includes(query.toLowerCase())
+  );
+  res.json(filteredProducts);
 });
 
-// Monta el enrutador de items
-app.use(itemsRouter);
+// Controlador para obtener detalles de un producto específico
+app.get('/api/items/:id', (req, res) => {
+  const productId = parseInt(req.params.id);
+  const product = products.products.find(product => product.id === productId);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: 'Producto no encontrado' });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor en ejecución en el puerto ${PORT}`);
